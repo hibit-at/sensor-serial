327b7e8bfce931d2a5a57354a87bcb30e7dbc500
hibit-at <hibit_at@hotmail.com>
2023-04-08 07:30:09 +0900

    local escape

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import serial
 import time
 import requests
+import json
+from local import URL_SEND_ENDPOINT,ACCESS_TOKEN
 
 ser = serial.Serial('COM3', 9600)
 time.sleep(2)
 
-url = "https://yourserver.com/your/api/endpoint"
-
 def send_post_request(data):
-    payload = {'value': data}
-    response = requests.post(url, data=payload)
-    print("Status code: ", response.status_code)
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Token {ACCESS_TOKEN}"
+    }
+    response = requests.post(URL_SEND_ENDPOINT, headers=headers, data=json.dumps({'value' : data}))
+
+    print("Status code: ", response.status_code,'data',data)
 
 start_time = time.time() # プログラム開始時のタイムスタンプを取得
-time_interval = 1 # 送信間隔（秒）
+time_interval = 10 # 送信間隔（秒）
+data_buffer = [] # データを一時的に格納するリスト
 
 try:
     while True:
         current_time = time.time() # 現在時刻のタイムスタンプを取得
         data = ser.readline().decode().strip()
+        data_buffer.append(float(data)) # バッファにデータを追加
         print("Received data: ", data)
-        if current_time - start_time >= time_interval: # 送信間隔が経過しているか確認
-            send_post_request(data) # POSTリクエストを送信
+        if current_time - start_time >= time_interval and data_buffer: # 送信間隔が経過しているか確認
+            avg_data = sum(data_buffer) / len(data_buffer) # バッファ内のデータの平均値を計算
+            send_post_request(avg_data) # POSTリクエストを送信
+            data_buffer = [] # バッファを空にする
             start_time = time.time() # 送信後、開始時刻を更新
 except KeyboardInterrupt:
     ser.close()
